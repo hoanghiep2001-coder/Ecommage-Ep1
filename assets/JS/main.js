2dfa4507cc0ffd516092911281b91714164ec962
unknown <duong.hoanghiepp@gmail.com>
2021-12-21 17:52:39 +0700

    This is Hiep

---
+++ assets/JS/main.js
@@ -0,0 +1,41 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const app = {
+    Galleries: [
+        {
+            path: 'gallery1',
+        },
+        {
+            path: 'gallery2',
+        },
+        {
+            path: 'gallery3',
+        },
+        {
+            path: 'gallery4',
+        },
+    ],
+    render: function() {
+        const htmls = this.Galleries.map(Gallery => {
+            return `
+            <div class="body__container-gallery-row-col col l-3">
+                <img src="./assets/Img/gallery/${Gallery.path}.png" alt="">
+                <h6>Ad eos saepe lucilius</h6>
+                <p>At eripuit signiferumque sea, vel ad mucius molestie, cu labitur.</p>
+            </div>
+            `;
+        })
+
+        $('.body__container-gallery-row').innerHTML = htmls.join("")
+    },
+    handleEvent: function () {
+
+    },
+    start: function() {
+        this.render()
+        this.handleEvent()
+    }
+}
+
+app.start()
